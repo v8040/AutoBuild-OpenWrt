e059499e0df1a9208c1946cf590696ac24591cd5
v8040 <59680407+v8040@users.noreply.github.com>
2022-10-23 14:56:24 +0800

    Add files via upload

--- scripts/diy/d2-lede.sh
+++ scripts/diy/d2-lede.sh
@@ -9,21 +9,10 @@ wget https://raw.githubusercontent.com/v8040/diy/main/lede-settings -O package/l
 orig_version=$(echo "$(cat package/lean/default-settings/files/zzz-default-settings)" | grep -Po "DISTRIB_REVISION=\'\K[^\']*")
 sed -i "s/${orig_version}/R$(TZ=UTC-8 date "+%-m.%-d")/g" package/lean/default-settings/files/zzz-default-settings
 sed -i '/exit 0/d' package/lean/default-settings/files/zzz-default-settings
-cat lede-settings >> zzz-default-settings
+cat package/lean/default-settings/files/lede-settings >> package/lean/default-settings/files/zzz-default-settings
 
 # 修改opkg源
 echo "src/gz openwrt_kiddin9 https://op.supes.top/packages/mipsel_24kc" >> package/system/opkg/files/customfeeds.conf
-
-# 移除重复package
-rm -rf feeds/luci/applications/luci-app-eqos
-rm -rf feeds/packages/net/v2ray-geodata
-
-# 添加package
-svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-eqos package/luci-app-eqos
-svn co https://github.com/kiddin9/openwrt-packages/trunk/v2ray-geodata package/v2ray-geodata
-
-# 调整菜单
-sed -i 's/network/control/g' package/luci-app-eqos/luasrc/controller/*.lua
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
