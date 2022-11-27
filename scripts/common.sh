82678e8a32887994cd7712d54e24de84a4c5ce5f
v8040 <59680407+v8040@users.noreply.github.com>
2022-11-27 13:44:49 +0800

    modify package
     modify package

--- scripts/common.sh
+++ scripts/common.sh
@@ -9,8 +9,6 @@ find . -maxdepth 4 -iname "*ddnsto*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*dockerman*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*music*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*onliner*" -type d | xargs rm -rf
-find . -maxdepth 4 -iname "*pushbot*" -type d | xargs rm -rf
-find . -maxdepth 4 -iname "*serverchan*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*speedtest*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*wrtbwmon*" -type d | xargs rm -rf
 
@@ -27,19 +25,23 @@ find . -maxdepth 4 -iname "*eqos*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*minidlna*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*netdata*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*openclash*" -type d | xargs rm -rf
+find . -maxdepth 4 -iname "*pushbot*" -type d | xargs rm -rf
+find . -maxdepth 4 -iname "*serverchan*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*turboacc*" -type d | xargs rm -rf
 find . -maxdepth 4 -iname "*verysync*" -type d | xargs rm -rf
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-minidlna package/luci-app-minidlna
-svn co https://github.com/coolsnowwolf/packages/trunk/multimedia/minidlna package/minidlna
 svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-turboacc package/luci-app-turboacc
+svn co https://github.com/coolsnowwolf/packages/trunk/multimedia/minidlna package/minidlna
 svn co https://github.com/kiddin9/openwrt-packages/trunk/dnsproxy package/dnsproxy
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-eqos package/luci-app-eqos
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netdata package/luci-app-netdata
-svn co https://github.com/kiddin9/openwrt-packages/trunk/netdata package/netdata
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-verysync package/luci-app-verysync
+svn co https://github.com/kiddin9/openwrt-packages/trunk/netdata package/netdata
 svn co https://github.com/kiddin9/openwrt-packages/trunk/verysync package/verysync
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
@@ -83,6 +85,11 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/lang\/golang\/golang\-package\.mk/include \$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang\-package\.mk/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHREPO/PKG_SOURCE_URL:=https:\/\/github\.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
 
 # 调整菜单
 sed -i 's/network/control/g' feeds/luci/applications/luci-app-sqm/luasrc/controller/*.lua
