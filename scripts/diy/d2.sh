531c3de0fc1ed266e8cacafad32f0e484c49dcb9
wufeifeng <wuyaos@users.noreply.github.com>
2023-10-27 22:11:43 +0800

    Update d2.sh

--- scripts/diy/d2.sh
+++ scripts/diy/d2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 修改默认IP和hostname
-sed -i 's/192.168.1.1/10.10.11.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.123.1/10.10.11.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/D2/g' package/base-files/files/bin/config_generate
 
 # 修改opkg源
