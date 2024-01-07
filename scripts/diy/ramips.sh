94c6cf8245f3a7f9aef180957e521265f3e2e56d
v8040 <59680407+v8040@users.noreply.github.com>
2024-01-07 23:48:07 +0800

    Switch to 23.05 branch

--- scripts/diy/ramips.sh
+++ scripts/diy/ramips.sh
@@ -2,7 +2,7 @@
 
 # 修改默认IP和hostname
 sed -i 's/192.168.1.1/10.10.11.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/D2/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 修改opkg源
 echo "src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/mipsel_24kc" >> package/system/opkg/files/customfeeds.conf
--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -20,9 +20,6 @@ uci commit system
 uci set nlbwmon.@nlbwmon[0].refresh_interval=2s
 uci commit nlbwmon
 
-
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
-
 sed -i 's/^src/#&/' /etc/opkg/distfeeds.conf
 sed -i 's/^option check_signature/#&/' /etc/opkg.conf
 
@@ -32,12 +29,5 @@ sed -i '/DISTRIB_RELEASE/d' /etc/openwrt_release
 echo "DISTRIB_RELEASE='$(TZ=UTC-8 date "+%Y.%-m.%-d")'" >> /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release
-
-sed -i 's/LuCI openwrt-18.06 branch/v8040/g' /usr/lib/lua/luci/version.lua
-sed -i 's/LuCI openwrt-18.06-k5.4 branch/v8040/g' /usr/lib/lua/luci/version.lua
-sed -i 's/LuCI openwrt-21.02 branch/v8040/g' /usr/lib/lua/luci/version.lua
-sed -i 's/LuCI openwrt-23.05 branch/v8040/g' /usr/lib/lua/luci/version.lua
-sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
-echo "luciversion = '$(TZ=UTC-8 date "+%Y.%-m.%-d")'" >> /usr/lib/lua/luci/version.lua
 
 exit 0
