eecba3f4bbc33df29341e0cb2025d64f3e376ff2
v8040 <59680407+v8040@users.noreply.github.com>
2025-11-01 04:42:51 +0800

    Clean up configs and scripts, update package defaults
    Removed development and toolchain options, IPv6, and various package selections from device configs for both immortalwrt-21.02 and 24.10. Updated GitHub Actions workflows to set 'flippy' as the default package method. Cleaned up scripts/common.sh by removing unnecessary package removals and additions, and adjusted sparse_clone and sed operations accordingly. These changes streamline build configurations and reduce unnecessary package management steps.

--- scripts/common.sh
+++ scripts/common.sh
@@ -46,8 +46,6 @@ info "[$(basename "${0}")] init"
 
 # Remove packages
 rm_pkg "*adguardhome"
-rm_pkg "*advanced"
-rm_pkg "*alist"
 rm_pkg "*amlogic"
 rm_pkg "*argon-config"
 rm_pkg "*bypass"
@@ -55,40 +53,26 @@ rm_pkg "*ddns-go"
 rm_pkg "*ddnsto"
 rm_pkg "*dockerman"
 rm_pkg "*mosdns"
-rm_pkg "*netdata"
-rm_pkg "*netspeedtest"
 rm_pkg "*nlbwmon*"
 rm_pkg "*onliner"
 rm_pkg "*openclash"
-rm_pkg "*partexp"
 rm_pkg "*passwall"
 rm_pkg "*pushbot"
 rm_pkg "*qbittorrent*"
 rm_pkg "*shadowsocks*"
-rm_pkg "*sqm*"
 rm_pkg "*ssr*"
-rm_pkg "*taskplan"
 rm_pkg "*theme-argon"
 rm_pkg "*transmission*"
 rm_pkg "*trojan*"
 rm_pkg "*v2ray*"
-rm_pkg "*wechatpush"
 rm_pkg "*xray*"
 rm_pkg "dnsproxy"
-rm_pkg "minidlna"
-rm_pkg "miniupnpc"
-rm_pkg "miniupnpd"
 
 # Add packages
 git clone -q --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone -q --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -q --depth=1 https://github.com/sbwml/luci-app-alist.git package/alist
 git clone -q --depth=1 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
 git clone -q --depth=1 https://github.com/sbwml/v2ray-geodata.git package/v2ray-geodata
-git clone -q --depth=1 https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-git clone -q --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
-git clone -q --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/luci-app-taskplan
-git clone -q --depth=1 https://github.com/sirpdboy/netspeedtest.git package/luci-app-netspeedtest
 git clone -q --depth=1 https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 
 sparse_clone main https://github.com/kiddin9/kwrt-packages.git luci-app-control-timewol
@@ -97,13 +81,7 @@ sparse_clone main https://github.com/linkease/nas-packages-luci.git luci/luci-ap
 sparse_clone main https://github.com/ophub/luci-app-amlogic.git luci-app-amlogic
 sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-ddns-go
 sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-dockerman
-sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-minidlna
-sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-sqm
-sparse_clone master https://github.com/immortalwrt/packages.git multimedia/minidlna
 sparse_clone master https://github.com/immortalwrt/packages.git net/ddns-go
-sparse_clone master https://github.com/immortalwrt/packages.git net/miniupnpc
-sparse_clone master https://github.com/immortalwrt/packages.git net/miniupnpd
-sparse_clone master https://github.com/immortalwrt/packages.git net/sqm-scripts
 sparse_clone master https://github.com/linkease/nas-packages.git network/services/ddnsto
 sparse_clone master https://github.com/vernesong/OpenClash.git luci-app-openclash
 
@@ -134,11 +112,10 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 sed -i 's/services/control/g' feeds/luci/applications/luci-app-eqos/root/usr/share/luci/menu.d/*.json
 sed -i 's/services/control/g' feeds/luci/applications/luci-app-nft-qos/luasrc/controller/*.lua
 sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/*.json
-sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-homeproxy/root/usr/share/luci/menu.d/*.json
 sed -i 's/services/vpn/g' package/luci-app-openclash/luasrc/controller/*.lua
 sed -i 's/services/vpn/g' package/luci-app-openclash/luasrc/model/cbi/openclash/*.lua
 sed -i 's/services/vpn/g' package/luci-app-openclash/luasrc/view/openclash/*.htm
-sed -i 's|admin/network|admin/control|g' package/luci-app-sqm/root/usr/share/luci/menu.d/*.json
+sed -i 's|admin/network|admin/control|g' feeds/luci/applications/luci-app-sqm/root/usr/share/luci/menu.d/*.json
 
 # Modify default IP and hostname
 sed -i "s|192\.168\.[0-9]*\.[0-9]*|${OPENWRT_IP}|g" feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
