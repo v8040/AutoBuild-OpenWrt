3f290f29fb50fcc0ed22660104324414fe7ab7a7
v8040 <59680407+v8040@users.noreply.github.com>
2025-11-01 19:23:24 +0800

    test

--- scripts/diy/immortalwrt-24.10/ramips.sh
+++ scripts/diy/immortalwrt-24.10/ramips.sh
@@ -48,7 +48,8 @@ info "[$(basename "${0}")] init"
 echo "src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/mipsel_24kc/kiddin9" >> package/system/opkg/files/customfeeds.conf
 
 # Add turboacc for firewall4
-curl -fsSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe &>/dev/null
+rm -rf package/network/utils/fullconenat-nft
+curl -fsSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh &>/dev/null
 
 sub_name "Turbo ACC 网络加速" "网络加速"
 sub_name "UPnP IGD 和 PCP" "UPnP设置"
